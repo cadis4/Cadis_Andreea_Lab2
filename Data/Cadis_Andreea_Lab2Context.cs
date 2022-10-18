3899161ef4955df4229111e53e13612acb758c4b
Andreea <Andreea@DESKTOP-MA30UTV>
2022-10-18 16:47:59 +0300

    Add project files.

---
+++ Data/Cadis_Andreea_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cadis_Andreea_Lab2.Models;
+
+namespace Cadis_Andreea_Lab2.Data
+{
+    public class Cadis_Andreea_Lab2Context : DbContext
+    {
+        public Cadis_Andreea_Lab2Context (DbContextOptions<Cadis_Andreea_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cadis_Andreea_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
